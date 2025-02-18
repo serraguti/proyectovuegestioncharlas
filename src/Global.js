27364a6cf27cefacb181387c206e238298d16676
serraguti <pacoserranox@gmail.com>
2025-02-18 13:46:11 +0100

    sa

--- src/Global.js
+++ src/Global.js
@@ -1,3 +1,4 @@
 export const Global = {
-    urlBase: 'https://apicharlasalumnostajamar2025.azurewebsites.net/',
+    //urlBase: 'https://apicharlasalumnostajamar2025.azurewebsites.net/',
+    urlBase: 'https://apicharlasalumnotajamar.azurewebsites.net/'
 };
